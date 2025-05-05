7e012be07fb98440b3bdfd4255e36c77e28d1ecd
Henrique-Palharim <henrique.dornelas@unesp.br>
2025-05-05 20:00:59 -0300

    update

--- Prova/Prova1Bi/frmMenu.Designer.cs
+++ Prova/Prova1Bi/frmMenu.Designer.cs
@@ -35,47 +35,56 @@
             // 
             // btTemperatura
             // 
+            this.btTemperatura.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btTemperatura.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btTemperatura.ForeColor = System.Drawing.Color.White;
             this.btTemperatura.Location = new System.Drawing.Point(195, 71);
-            this.btTemperatura.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.btTemperatura.Margin = new System.Windows.Forms.Padding(7);
             this.btTemperatura.Name = "btTemperatura";
             this.btTemperatura.Size = new System.Drawing.Size(208, 180);
             this.btTemperatura.TabIndex = 0;
             this.btTemperatura.Text = "&Temperatura";
-            this.btTemperatura.UseVisualStyleBackColor = true;
+            this.btTemperatura.UseVisualStyleBackColor = false;
             this.btTemperatura.Click += new System.EventHandler(this.btTemperatura_Click);
             // 
             // btMedia
             // 
+            this.btMedia.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btMedia.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btMedia.ForeColor = System.Drawing.Color.White;
             this.btMedia.Location = new System.Drawing.Point(488, 71);
             this.btMedia.Margin = new System.Windows.Forms.Padding(7);
             this.btMedia.Name = "btMedia";
             this.btMedia.Size = new System.Drawing.Size(208, 180);
             this.btMedia.TabIndex = 1;
             this.btMedia.Text = "&Media";
-            this.btMedia.UseVisualStyleBackColor = true;
+            this.btMedia.UseVisualStyleBackColor = false;
             this.btMedia.Click += new System.EventHandler(this.btMedia_Click);
             // 
             // btSair
             // 
-            this.btSair.Location = new System.Drawing.Point(853, 346);
+            this.btSair.BackColor = System.Drawing.Color.Crimson;
+            this.btSair.ForeColor = System.Drawing.Color.White;
+            this.btSair.Location = new System.Drawing.Point(761, 320);
             this.btSair.Margin = new System.Windows.Forms.Padding(7);
             this.btSair.Name = "btSair";
             this.btSair.Size = new System.Drawing.Size(118, 37);
             this.btSair.TabIndex = 2;
             this.btSair.Text = "&Sair";
-            this.btSair.UseVisualStyleBackColor = true;
+            this.btSair.UseVisualStyleBackColor = false;
             this.btSair.Click += new System.EventHandler(this.btSair_Click);
             // 
             // frmMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(987, 399);
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(908, 379);
             this.Controls.Add(this.btSair);
             this.Controls.Add(this.btMedia);
             this.Controls.Add(this.btTemperatura);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Margin = new System.Windows.Forms.Padding(7);
             this.Name = "frmMenu";
             this.Text = "Menu de Aplicações";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMenu_FormClosing);
